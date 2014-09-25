b9cbb025e7d4eb96719e0e308de8011e42c8debd
Rep Woroch <rep_woroch@asl.org>
2014-09-25 09:25:04 +0100

    Made a working program.

--- TemperatureChanger.java
+++ TemperatureChanger.java
@@ -10,9 +10,43 @@ import java.util.Scanner;
 public class TemperatureChanger {
 
   public static void main (String[] args) {
-    // TODO - scan for input in the format 175F or 18C 
-    // TODO - perform correct conversion
-    // TODO - print out results to the screen
+
+	double result = 0;
+
+	//Create the Scanner and assign the next input to a string.
+
+	Scanner scnr = new Scanner(System.in);
+	System.out.println("Enter a number followed by a unit to convert to the other. Ex: 32F...");
+	String input = scnr.next();
+
+	
+	//Test if the input ends with C or F
+
+	if (input.charAt(input.length()-1) == 'C') {
+		
+		//Remove the type and expose the number.
+		String number = input.substring(0, input.length()-1);
+
+		//Convert to a double.
+		double inputNumber = Double.parseDouble(number);
+		
+		//Perform conversion and print out the result.
+		result = (((inputNumber*9)/5) + 32);
+		System.out.println(result+"F");
+	}
+
+	if (input.charAt(input.length()-1) == 'F') {
+
+		//Remove the type and expose the number.		
+		String number = input.substring(0, input.length()-1);
+
+		//Convert to a double.
+		double inputNumber = Double.parseDouble(number);
+
+		//Perform conversion and print out the result.
+		result = (((inputNumber-32)*5)/9);
+		System.out.println(result+"C"); 
+	}
   }
   
 }
